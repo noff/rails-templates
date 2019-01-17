f613e1eb7d0ba1b9695e6f1bd89bb6d8f61ddb11
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2019-01-17 09:47:57 +0300

    mostly done

--- common.rb
+++ common.rb
@@ -1,3 +1,98 @@
+# Init variables with file content
+
+capfile <<-TEXT
+# Load DSL and set up stages
+require "capistrano/setup"
+
+# Include default deployment tasks
+require "capistrano/deploy"
+
+# Load the SCM plugin appropriate to your project:
+#
+# require "capistrano/scm/hg"
+# install_plugin Capistrano::SCM::Hg
+# or
+# require "capistrano/scm/svn"
+# install_plugin Capistrano::SCM::Svn
+# or
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+# Include tasks from other gems included in your Gemfile
+#
+# For documentation on these, see for example:
+#
+#   https://github.com/capistrano/rvm
+#   https://github.com/capistrano/rbenv
+#   https://github.com/capistrano/chruby
+#   https://github.com/capistrano/bundler
+#   https://github.com/capistrano/rails
+#   https://github.com/capistrano/passenger
+#
+require "capistrano/rvm"
+require "capistrano/bundler"
+require "capistrano/rails/assets"
+require "capistrano/rails/migrations"
+require 'capistrano/puma'
+install_plugin Capistrano::Puma
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
+
+TEXT
+
+database_yml <<-TEXT
+default: &default
+  adapter: postgresql
+  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+  timeout: 5000
+
+development:
+  <<: *default
+  database: db_development
+
+test:
+  <<: *default
+  database: db_test
+
+production:
+  <<: *default
+  database: db_production
+TEXT
+
+application_slim <<-TEXT
+doctype html
+html
+  head
+    title = content_for?(:title) ? yield(:title) : 'My Project'
+    meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"
+
+    / Twitter
+    meta name="twitter:card" content="summary"
+    meta name="twitter:title" content="#{content_for?(:title) ? yield(:title) : 'My Project'}"
+    meta name="twitter:description" content="#{content_for?(:description) ? yield(:description) : 'Shared description'}"
+
+    / Facebook
+    meta property="og:site_name" content="My Project"
+    meta property="og:title" content="#{content_for?(:title) ? yield(:title) : 'My Project'}"
+    meta property="og:description" content="#{content_for?(:description) ? yield(:description) : 'Shared description'}"
+
+    / Other
+    title = content_for?(:title) ? yield(:title) : 'My Project'
+    meta name="description" content="#{content_for?(:description) ? yield(:description) : 'Shared description'}"
+
+    = csrf_meta_tags
+    = csp_meta_tag
+    link rel="icon" href="/favicon.ico"
+    = stylesheet_link_tag    'application', media: 'all'
+    = javascript_include_tag 'application'
+    link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"
+
+  body
+    = yield
+TEXT
+
+
 gem 'pg'
 gem 'puma', '~> 3.11'
 gem 'sassc-rails'
@@ -10,6 +105,7 @@ gem 'jquery-rails'
 gem 'activeadmin'
 gem 'devise'
 gem 'rollbar'
+gem 'whenever', require: false
 
 gem_group :development do
   gem 'capistrano', '~> 3.11.0'
@@ -27,15 +123,64 @@ end
 run "bundle"
 
 after_bundle do
+
+  # rspec
+  rails_command "generate rspec:install"
+
+  # devise
+  rails_command "generate devise:install"
+  environment 'config.action_mailer.default_url_options = {host: "http://yourwebsite.example.com"}', env: 'production'
+  environment 'config.action_mailer.default_url_options = {host: "localhost", port: 3000}', env: 'development'
+
+  # whenever
+  run "wheneverize ."
+
+  # bootstrap
+  run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.scss"
+  run "echo '@import \"bootstrap\";' > app/assets/stylesheets/application.scss"
+  run "echo '//= require jquery' > app/assets/javascripts/application.js"
+  run "echo '//= require rails-ujs' >> app/assets/javascripts/application.js"
+  run "echo '//= require activestorage' >> app/assets/javascripts/application.js"
+  run "echo '//= require popper' >> app/assets/javascripts/application.js"
+  run "echo '//= require bootstrap' >> app/assets/javascripts/application.js"
+  run "echo '//= require_tree .' >> app/assets/javascripts/application.js"
+
+  # capistrano
+  run "bundle exec cap install"
+  File.open('Capfile', 'w') { |file| file.write capfile }
+  # run "echo 'require \"capistrano/setup\"' > Capfile"
+  # run "echo 'require \"capistrano/deploy\"' >> Capfile"
+  # run "echo 'require \"capistrano/scm/git\"' >> Capfile"
+  # run "echo 'install_plugin Capistrano::SCM::Git' >> Capfile"
+  # run "echo 'require \"capistrano/rvm\"' >> Capfile"
+  # run "echo 'require \"capistrano/bundler\"' >> Capfile"
+  # run "echo 'require \"capistrano/rails/assets\"' >> Capfile"
+  # run "echo 'require \"capistrano/rails/migrations\"' >> Capfile"
+  # run "echo 'require \"capistrano/puma\"' >> Capfile"
+  # run "echo 'install_plugin Capistrano::Puma' >> Capfile"
+  # run "echo 'Dir.glob(\"lib/capistrano/tasks/*.rake\").each { |r| import r }' >> Capfile"
+
+  # database
+  File.open('config/database.yml', 'w') { |file| file.write database_yml }
+  run "rm db/*.sqlite3"
+
+  # layout
+  File.open('app/views/layouts/application.slim', 'w') { |file| file.write application_slim }
+  run "rm app/views/layouts/application.hmtl.erb"
+
+  # root controller
+  generate(:controller, "welcome", "index", "--skip-routes", "--no-helper")
+  route "root to: 'welcome#index'"
+
+  # active_admin
+  rails_command "generate active_admin:install"
+  # rails_command "generate active_admin:resource User"
+
   rails_command "db:migrate"
 
-  # Install bootstrap
-  # Install devise
-  # Install capistrano
-  # Install DB config
-  # Convert application slim
-  # Generate root controller
-  # Generate Users resource for ActiveAdmin
+  # gitignore
+  run "echo '.idea' >> .gitignore"
+  run "echo '*.iml' >> .gitignore"
 
   git :init
   git add: "."
@@ -45,8 +190,3 @@ end
 
 
 
-
-
-
-
-# https://github.com/twbs/bootstrap-rubygem
